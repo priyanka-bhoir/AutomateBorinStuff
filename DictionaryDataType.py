13c8f4e2039dec2a3eef3e3ac9884a7a0ed5f437
Priyanka <priyanka.pb106@yahoo.com>
2021-10-21 20:23:43 +0530

    dic part 1

---
+++ DictionaryDataType.py
@@ -0,0 +1,13 @@
+# dictionaries have no order
+
+eggs = {'name':'zophie','spices':'cat','age':8}
+ham = {'spices':'cat','name':'zophie','age':8}
+
+print(eggs == ham)
+
+#
+
+print("name" in eggs)
+
+# dictionaries are mutable
+
