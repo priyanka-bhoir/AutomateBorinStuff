f88c041b5479e8bc5ad3de102c295c17ab2a8aac
Priyanka <priyanka.pb106@yahoo.com>
2021-10-21 20:57:56 +0530

    charcountfun

--- DictionaryDataType.py
+++ DictionaryDataType.py
@@ -10,4 +10,17 @@ print(eggs == ham)
 print("name" in eggs)
 
 # dictionaries are mutable
+print(list(eggs.keys()))
 
+print(list(eggs.values()))
+
+print(list(eggs.items()))# this will return tuples
+
+# get work as turnerry operator
+
+print(eggs.get("name","l"))
+print(eggs.get("cat","l"))
+
+# setdefault method
+print(eggs.setdefault('color','black'))
+print(eggs)
